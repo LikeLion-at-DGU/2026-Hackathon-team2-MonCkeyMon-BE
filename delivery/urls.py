d05a08c80ae425d6daf9d057c1ca753ce217b830
khxxn <kyohyun58@naver.com>
2026-08-13 13:35:25 +0900

    Feat: 완성된 동영상 get api

--- delivery/urls.py
+++ delivery/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
+from .views import ShareDetailView
 
-# ⚠️ 변수명이 정확히 소문자 'urlpatterns' 이고 리스트([]) 형태여야 합니다!
 urlpatterns = [
-    # 비어있더라도 대괄호([]) 리스트 형태여야 에러가 안 납니다.
+    path('share/<uuid:session_id>/', ShareDetailView.as_view(), name='share-detail'),
 ]
