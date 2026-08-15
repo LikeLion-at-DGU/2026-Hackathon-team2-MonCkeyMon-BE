747aa95b230cf431cd735019dac9f6dc6e1f5485
khxxn <kyohyun58@naver.com>
2026-08-15 23:27:32 +0900

    Fix: 분석페이지 serializers.py랑 views.py랑 나누기

--- analytics/views.py
+++ analytics/views.py
@@ -1,63 +1,60 @@
-from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from products.models import Product, Background
 
+from .serializers import (
+    ProductChooseCountSerializer,
+    BackgroundChooseCountSerializer,
+    ProductLikeCountSerializer,
+)
+
 
 class ChooseCountView(APIView):
 
     def get(self, request):
-        products = Product.objects.values(
-            'id',
-            'name',
-            'choose_count'
-        )
+        products = ProductChooseCountSerializer(
+            Product.objects.all(),
+            many=True
+        ).data
 
-        backgrounds = Background.objects.values(
-            'id',
-            'name',
-            'choose_count'
-        )
+        backgrounds = BackgroundChooseCountSerializer(
+            Background.objects.all(),
+            many=True
+        ).data
 
         return Response({
             'products': products,
             'backgrounds': backgrounds,
         })
+
 
 class ChooseCountTop5View(APIView):
 
     def get(self, request):
-        products = Product.objects.order_by(
-            '-choose_count'
-        ).values(
-            'id',
-            'name',
-            'choose_count'
-        )[:5]
+        products = ProductChooseCountSerializer(
+            Product.objects.order_by('-choose_count')[:5],
+            many=True
+        ).data
 
-        backgrounds = Background.objects.order_by(
-            '-choose_count'
-        ).values(
-            'id',
-            'name',
-            'choose_count'
-        )[:5]
+        backgrounds = BackgroundChooseCountSerializer(
+            Background.objects.order_by('-choose_count')[:5],
+            many=True
+        ).data
 
         return Response({
             'products': products,
             'backgrounds': backgrounds,
         })
 
+
 class ProductLikeCountView(APIView):
 
     def get(self, request):
-        products = Product.objects.values(
-            'id',
-            'name',
-            'like_count'
-        )
+        products = ProductLikeCountSerializer(
+            Product.objects.all(),
+            many=True
+        ).data
 
         return Response({
             'products': products,
