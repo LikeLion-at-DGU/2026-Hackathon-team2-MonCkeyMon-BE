9d59431107c8bc7346584a9c455cefcfc4e88dca
khxxn <kyohyun58@naver.com>
2026-08-20 08:57:32 +0900

    Fix: 코드최적화

--- delivery/urls.py
+++ delivery/urls.py
@@ -2,14 +2,6 @@ from django.urls import path
 from .views import ShareDetailView, CompositeImageView
 
 urlpatterns = [
-    path(
-        'share/<uuid:session_id>/',
-        ShareDetailView.as_view(),
-        name='share-detail'
-    ),
-    path(
-        'composite/<uuid:session_id>/',
-        CompositeImageView.as_view(),
-        name='composite-image'
-    ),
+    path('share/<uuid:session_id>/', ShareDetailView.as_view(), name='share-detail'),
+    path('composite/<uuid:session_id>/', CompositeImageView.as_view(), name='composite-image'),
 ]
